51f1926f580a3e8c9bd1b9dcb204de492aa17384
Ohad Shirazi <92723105+ohad1s@users.noreply.github.com>
2022-04-14 18:08:46 +0300

    fixed mistakes

--- sources/Matrix.cpp
+++ sources/Matrix.cpp
@@ -26,15 +26,15 @@ namespace zich {
         }
         vector<double> addition;
         for (int i = 0; i < this->my_matrix.size(); i++) {
-            addition[i] = this->my_matrix[i] + mat.my_matrix[i];
+            addition[(unsigned int) i] = this->my_matrix[(unsigned int) i] + mat.my_matrix[(unsigned int) i];
         }
-        new_mat = Matrix::Matrix(addition, this->row, this->col);
+        Matrix new_mat(addition, this->row, this->col);
         return new_mat;
     }
 
     Matrix &Matrix::operator++() {
         for (int i = 0; i < this->my_matrix.size(); i++) {
-            this->my_matrix[i] += 1;
+            this->my_matrix[(unsigned int) i] += 1;
         }
         return *this;
     }
@@ -42,7 +42,7 @@ namespace zich {
     Matrix Matrix::operator++(const int num) {
         Matrix copy = *this;
         for (int i = 0; i < this->my_matrix.size(); i++) {
-            this->my_matrix[i] += 1;
+            this->my_matrix[(unsigned int) i] += 1;
         }
         return copy;
     }
@@ -52,7 +52,7 @@ namespace zich {
             throw std::invalid_argument("matrices have different size");
         }
         for (int i = 0; i < this->my_matrix.size(); i++) {
-            this->my_matrix[i] = this->my_matrix[i] + mat.my_matrix[i];
+            this->my_matrix[(unsigned int) i] = this->my_matrix[(unsigned int) i] + mat.my_matrix[(unsigned int) i];
         }
         return *this;
     }
@@ -63,153 +63,151 @@ namespace zich {
         }
         vector<double> subtraction;
         for (int i = 0; i < this->my_matrix.size(); i++) {
-            addition[i] = this->my_matrix[i] - mat.my_matrix[i];
+            subtraction[(unsigned int) i] = this->my_matrix[(unsigned int) i] - mat.my_matrix[(unsigned int) i];
         }
-        new_mat = Matrix::Matrix(subtraction, this->row, this->col);
+        Matrix new_mat(subtraction, this->row, this->col);
         return new_mat;
     }
-}
 
-Matrix &Matrix::operator--() {
-    for (int i = 0; i < this->my_matrix.size(); i++) {
-        this->my_matrix[i] -= 1;
+
+    Matrix &Matrix::operator--() {
+        for (int i = 0; i < this->my_matrix.size(); i++) {
+            this->my_matrix[(unsigned int) i] -= 1;
+        }
+        return *this;
     }
-    return *this;
-}
 
-Matrix Matrix::operator--(const int num) {
-    Matrix copy = *this;
-    for (int i = 0; i < this->my_matrix.size(); i++) {
-        this->my_matrix[i] -= 1;
+    Matrix Matrix::operator--(const int num) {
+        Matrix copy = *this;
+        for (int i = 0; i < this->my_matrix.size(); i++) {
+            this->my_matrix[(unsigned int) i] -= 1;
+        }
+        return copy;
     }
-    return copy;
-}
 
-Matrix &Matrix::operator-=(const Matrix &mat) {
-    if (this->row != mat.row || this->col != this->col) {
-        throw std::invalid_argument("matrices have different size");
+    Matrix &Matrix::operator-=(const Matrix &mat) {
+        if (this->row != mat.row || this->col != this->col) {
+            throw std::invalid_argument("matrices have different size");
+        }
+        for (int i = 0; i < this->my_matrix.size(); i++) {
+            this->my_matrix[(unsigned int) i] = this->my_matrix[(unsigned int) i] - mat.my_matrix[(unsigned int) i];
+        }
+        return *this;
     }
-    for (int i = 0; i < this->my_matrix.size(); i++) {
-        this->my_matrix[i] = this->my_matrix[i] - mat.my_matrix[i];
+
+    Matrix Matrix::operator*(const Matrix &mat) {
     }
-    return *this;
-}
 
-Matrix Matrix::operator*(const Matrix &mat) {
-}
-
-Matrix &Matrix::operator*=(const Matrix &mat) {
-    return *this;
-}
-
-Matrix Matrix::operator*(const double scalar) {
-    vector<double> mul;
-    for (int i = 0; i < this->my_matrix.size(); i++) {
-        mul[i] = this->my_matrix[i] * scalar;
+    Matrix &Matrix::operator*=(const Matrix &mat) {
+        return *this;
     }
-    new_mat = Matrix::Matrix(mul, this->row, this->col);
-    return new_mat;
-}
 
-Matrix &Matrix::operator*=(const double scalar) {
-    for (int i = 0; i < this->my_matrix.size(); i++) {
-        this->my_matrix[i] = this->my_matrix[i] * scalar;
+    Matrix Matrix::operator*(const double scalar) {
+        vector<double> mul;
+        for (int i = 0; i < this->my_matrix.size(); i++) {
+            mul[(unsigned int) i] = this->my_matrix[(unsigned int) i] * scalar;
+        }
+        Matrix new_mat(mul, this->row, this->col);
+        return new_mat;
     }
-    return *this;
-}
 
-bool Matrix::operator>(Matrix &mat) {
-    if (this->row != mat.row || this->col != this->col) {
-        throw std::invalid_argument("matrices have different size");
+    Matrix &Matrix::operator*=(const double scalar) {
+        for (int i = 0; i < this->my_matrix.size(); i++) {
+            this->my_matrix[(unsigned int) i] = this->my_matrix[(unsigned int) i] * scalar;
+        }
+        return *this;
     }
-    double sum_this = 0.0;
-    double sum_other = 0.0;
-    for (int i = 0; i < this->my_matrix.size(); i++) {
-        sum_this+=this->my_matrix[i];
-        sum_other+=mat->my_matrix[i];
+
+    bool Matrix::operator>(Matrix &mat) {
+        if (this->row != mat.row || this->col != this->col) {
+            throw std::invalid_argument("matrices have different size");
+        }
+        double sum_this = 0.0;
+        double sum_other = 0.0;
+        for (int i = 0; i < this->my_matrix.size(); i++) {
+            sum_this += this->my_matrix[(unsigned int) i];
+            sum_other += mat.my_matrix[(unsigned int) i];
+        }
+        return sum_this > sum_other;
     }
-    return sum_this>sum_other;
-}
 
-bool Matrix::operator>=(Matrix &mat) {
-    if (this->row != mat.row || this->col != this->col) {
-        throw std::invalid_argument("matrices have different size");
+    bool Matrix::operator>=(Matrix &mat) {
+        if (this->row != mat.row || this->col != this->col) {
+            throw std::invalid_argument("matrices have different size");
+        }
+        double sum_this = 0.0;
+        double sum_other = 0.0;
+        for (int i = 0; i < this->my_matrix.size(); i++) {
+            sum_this += this->my_matrix[(unsigned int) i];
+            sum_other += mat.my_matrix[(unsigned int) i];
+        }
+        return sum_this >= sum_other;
     }
-    double sum_this = 0.0;
-    double sum_other = 0.0;
-    for (int i = 0; i < this->my_matrix.size(); i++) {
-        sum_this+=this->my_matrix[i];
-        sum_other+=mat->my_matrix[i];
+
+    bool Matrix::operator<(Matrix &mat) {
+        if (this->row != mat.row || this->col != this->col) {
+            throw std::invalid_argument("matrices have different size");
+        }
+        double sum_this = 0.0;
+        double sum_other = 0.0;
+        for (int i = 0; i < this->my_matrix.size(); i++) {
+            sum_this += this->my_matrix[(unsigned int) i];
+            sum_other += mat.my_matrix[(unsigned int) i];
+        }
+        return sum_this < sum_other;
     }
-    return sum_this>=sum_other;
-}
 
-bool Matrix::operator<(Matrix &mat) {     if (this->row != mat.row || this->col != this->col) {
-        throw std::invalid_argument("matrices have different size");
+    bool Matrix::operator<=(Matrix &mat) {
+        if (this->row != mat.row || this->col != this->col) {
+            throw std::invalid_argument("matrices have different size");
+        }
+        double sum_this = 0.0;
+        double sum_other = 0.0;
+        for (int i = 0; i < this->my_matrix.size(); i++) {
+            sum_this += this->my_matrix[(unsigned int) i];
+            sum_other += mat.my_matrix[(unsigned int) i];
+        }
+        return sum_this <= sum_other;
     }
-    double sum_this = 0.0;
-    double sum_other = 0.0;
-    for (int i = 0; i < this->my_matrix.size(); i++) {
-        sum_this+=this->my_matrix[i];
-        sum_other+=mat->my_matrix[i];
+
+
+    std::ostream &operator<<(std::ostream &output, const Matrix &m) {
+        return output;
     }
-    return sum_this<sum_other; }
 
-bool Matrix::operator<=(Matrix &mat) {     if (this->row != mat.row || this->col != this->col) {
-        throw std::invalid_argument("matrices have different size");
+    std::istream &operator>>(std::istream &input, Matrix &m) {
+        return input;
     }
-    double sum_this = 0.0;
-    double sum_other = 0.0;
-    for (int i = 0; i < this->my_matrix.size(); i++) {
-        sum_this+=this->my_matrix[i];
-        sum_other+=mat->my_matrix[i];
+
+    bool operator==(const Matrix &m1, const Matrix &m2) {
+        if (m1.row != m2.row || m1.col != m2.col) {
+            throw std::invalid_argument("matrices have different size");
+        }
+        for (int i = 0; i < m1.my_matrix.size(); i++) {
+            int d_this = m1.my_matrix[(unsigned int) i];
+            int d_other = m2.my_matrix[(unsigned int) i];
+            if (d_this != d_other) {
+                return false;
+            }
+        }
+        return true;
     }
-    return sum_this<=sum_other; }
 
-
-std::ostream &operator<<(std::ostream &output, const Matrix &m) {
-    return output;
-}
-
-std::istream &operator>>(std::istream &input, Matrix &m) {
-    return input;
-}
-
-bool operator==(const Matrix &m1, const Matrix &m2) {     if (this->row != mat.row || this->col != this->col) {
-        throw std::invalid_argument("matrices have different size");
+    bool operator!=(const Matrix &m1, const Matrix &m2) {
+        return !(m1 == m2);
     }
-    double sum_this = 0.0;
-    double sum_other = 0.0;
-    for (int i = 0; i < this->my_matrix.size(); i++) {
-        sum_this+=this->my_matrix[i];
-        sum_other+=mat->my_matrix[i];
+
+    Matrix operator-(Matrix &mat) {
+
     }
-    return sum_this==sum_other; }
 
-bool operator!=(const Matrix &m1, const Matrix &m2) {
-    if (this->row != mat.row || this->col != this->col) {
-        throw std::invalid_argument("matrices have different size");
+    Matrix operator+(Matrix &mat) {
+
     }
-    double sum_this = 0.0;
-    double sum_other = 0.0;
-    for (int i = 0; i < this->my_matrix.size(); i++) {
-        sum_this+=this->my_matrix[i];
-        sum_other+=mat->my_matrix[i];
+
+    Matrix operator*(const double scalar, Matrix &mat) {
+
     }
-    return sum_this!=sum_other;
-}
-
-Matrix operator-(Matrix &mat) {
-
-}
-
-Matrix operator+(Matrix &mat) {
-
-}
-
-Matrix operator*(const double scalar, Matrix &mat) {
-
-}
 
 
 }
